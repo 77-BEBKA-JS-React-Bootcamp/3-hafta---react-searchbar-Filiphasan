ebbda876d759ed9bf51dfbd376d3f2270aca1faa
Hasan Erdal <58440290+Filiphasan@users.noreply.github.com>
2021-03-06 23:20:08 +0300

    update appjs

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import logo from './icons8_university.svg';
 import './App.css';
 
 export class App extends Component {
-  constructor(){
-    super();
-    this.getData=this.getData.bind(this);
-    this.onChangeFilter=this.onChangeFilter.bind(this);
+  constructor(props) {
+    super(props);
+    this.getData = this.getData.bind(this);
+    this.onChangeFilter = this.onChangeFilter.bind(this);
   }
   state = {
-    filter:'',
+    filter: '',
     datas: []
   }
   getData() {
     fetch("http://universities.hipolabs.com/search?country=Turkey")
       .then((res) => res.json())
-      .then((da) => {this.setState({datas:da});});
+      .then((datas) => { this.setState({ datas }); });
   }
-  onChangeFilter(e){
+  onChangeFilter(e) {
     this.setState({
       filter: e.target.value
     });
@@ -25,8 +25,8 @@ export class App extends Component {
   render() {
     this.getData();
     const filteredDatas = this.state.datas.filter(
-      data =>{
-        return data.name.toLowerCase().indexOf(this.state.filter.toLowerCase()) !==-1;
+      data => {
+        return data.name.toLowerCase().indexOf(this.state.filter.toLowerCase()) !== -1;
       }
     );
     return (
@@ -40,22 +40,22 @@ export class App extends Component {
             </div>
           </header>
           <section id="university-cards">
-            {filteredDatas.map(data =>{
-              return(
-                <div class="university-container">
-                <div class="university">
+            {filteredDatas.map(data => {
+              return (
+                <div  class="university-container">
+                  <div class="university">
                     <div class="university-preview">
-                        <h6>NAME</h6>
-                        <h2><a href={data.web_pages[0]}>{data.name}</a></h2>
+                      <h6>NAME</h6>
+                      <h2><a href={data.web_pages[0]}>{data.name}</a></h2>
                     </div>
                     <div class="university-info">
-                        <h6>LOCATION</h6>
-                        <h2>{data.country}</h2>
-                        <h6>DOMAIN</h6>
-                        <h2>{data.domains[0]}</h2>
+                      <h6>LOCATION</h6>
+                      <h2>{data.country}</h2>
+                      <h6>DOMAIN</h6>
+                      <h2>{data.domains[0]}</h2>
                     </div>
+                  </div>
                 </div>
-            </div>
               );
             })}
           </section>
